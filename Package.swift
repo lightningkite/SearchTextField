90e20fe125cc6461dafbd1a51f1970fea28838ad
Shane Thompson <shane@meosphere.com>
2025-04-30 10:28:39 -0600

    Updated to use swift package manager

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "SearchTextField",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(name: "SearchTextField", targets: ["SearchTextField"]),
+    ],
+    dependencies: []
+    targets: [
+        .target(
+            name: "SearchTextField",
+            dependencies: [],
+            path: "Sources/SearchTextField",
+            publicHeadersPath: "include",
+        )
+    ]
+)
