4f5aa095698f90d17c87531f1bcbb23caa6cb7f7
Shane Thompson <shane@meosphere.com>
2025-04-30 14:43:08 -0600

    Fixed error in Package.Swift file

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(name: "SearchTextField", targets: ["SearchTextField"]),
     ],
-    dependencies: []
+    dependencies: [],
     targets: [
         .target(
             name: "SearchTextField",
